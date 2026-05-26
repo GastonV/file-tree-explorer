f999e66f2189d7f952856e5e9472146fab1c2ef8
Atis Simrais <atis.sirmais@gmail.com>
2026-05-26 14:29:45 +0300

    fix: remove unused app.routes import from app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,10 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
     provideAnimations(),
     provideHttpClient()
   ]
